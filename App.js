e91d6a4b5b62c2c6a03b20166bf0c349941b2fab
Yurii Oliiarnyk <oliiarnyk14@gmail.com>
2019-12-05 16:16:17 +0200

    init project

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  text: {
+    fontSize: 32
+  }
+});
